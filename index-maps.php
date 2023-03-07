34175e83cf57389cfb61f31144aea8a1853848ba
Marina ASHRAF MORIS <119459259+Marina-ASH@users.noreply.github.com>
2023-03-07 17:27:56 +0100

    main

--- index-maps.php
+++ index-maps.php
@@ -4,7 +4,6 @@
             <?php
                 $api_key = "prj_live_sk_5014c0483ac78ccca7ea5a847c0de989446ae828";
                 $curl = curl_init();
-                curl "https://api.radar.io/v1/geocode/forward?query=20+jay+st+brooklyn+ny" \-H "Authorization: prj_live_pk_...";
                 curl_setopt($curl, CURLOPT_URL, "https://services.marinetraffic.com/api/exportvesseltrack/v:3/10617f57ee871819862ca005ccadefc45827d462/period:daily/days:10/mmsi:".$id."/protocol:xml");
                 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         
