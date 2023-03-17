c91b42264dc5ea8e9910cd0ef801c37503e066db
Marina ASHRAF MORIS <119459259+Marina-ASH@users.noreply.github.com>
2023-03-17 12:16:36 +0100

    ADD  BDD

--- test.php
+++ test.php
@@ -3,22 +3,28 @@
     <pre>
 
       <?php
-         $api_key = "9de816e3e2d89da6031822d6a82906c199c3378e";
-         $tableauBateauxMMSI = ["224001900" => 'Atyla', "228061110" => 'Bel Espoir', "228796000" => 'Belle Poule', "770576100" => 'Capitan Miranda', "205208000" => 'Crocus', "228797000" => 'Etoile', "228000700" => 'Etoile Du Roy', "227731000" => 'Etoile Molene', "8650796" => 'Hydrograaf', "227306100" => 'La Recouvrance', "205209000" => 'Lobelia', "227806500" => 'Mutin', "224123770" => 'Nao Victoria', "224534350" => 'Pascual Flores', "273452840" =>'Shtandart', "263804290" => 'VERA CRUZ'];
-         $tableauBateauxIMO = ["8622983" => 'Belem', "8333635" => 'Atlantis', "9792319" => 'Bima Suci', "8107505" => '8107505', "7821075" => 'Dar Mlodziezy', "9648506" => 'Jeanne Barret', "5183120" => 'Le Francais', "5225514" => 'Marite', "5241659" => 'Morgenster', "5312628" => 'Santa Maria Manuela', "5339248" => 'STATSRAAD LEHMKUHL', "8101276" => 'Thalassa' ];
+        // Génère un code unique pour chaque jour en utilisant la date du jour
+        $code = "CODE-" . date('Y-m-d');
+
+        // Affiche le code généré pour le jour en cours
+        echo $code;
         
-         $servername ="localhost";
-         $username = "armada";
-         $password = "btssnir";
-         $dbname = "armadaproj";
+        $api_key = "9de816e3e2d89da6031822d6a82906c199c3378e";
+        $tableauBateauxMMSI = ["228000700" => 'Etoile Du Roy'];
+        $tableauBateauxIMO = ["7821075" => 'DAR MLODZIEZY'];
+        $servername ="localhost";
+        $username = "armada";
+        $password = "btssnir";
+        $dbname = "armadaproj";
  
-         // Connexion à la base de données
-         $conn = mysqli_connect($servername, $username, $password, $dbname);
-         if (!$conn) {
+        // Connexion à la base de données
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+        if (!$conn) {
            die("Connection failed: " . mysqli_connect_error());
-         }
+        }
  
-         foreach ($tableauBateauxMMSI as $idMMSI => $nameMMSI){
+        foreach ($tableauBateauxMMSI as $idMMSI => $nameMMSI){
            $curl = curl_init();
            curl_setopt($curl, CURLOPT_URL, "https://services.marinetraffic.com/api/exportvesseltrack/v:3/$api_key/period:daily/days:1/mmsi:$idMMSI/protocol:xml");
            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -51,7 +57,7 @@
              {
                 echo "Error: " . $sql . "<br>" . mysqli_error($conn);
              }
-         }
+        }
         
 
        
@@ -69,7 +75,7 @@
           $xml = json_decode($xml, true);
 
           if (empty($xml)){
-            continue;
+             continue;
           }
 
           $xml = $xml  ['POSITION'];
@@ -78,7 +84,6 @@
           echo $item['MMSI'] . "     :     " . $item['LON'] . "    :   " . $item['LAT'] .  "   :   " . $item['TIMESTAMP']. "   :   " . $nameIMO."<br>";
                    
           $sql = "INSERT INTO bateau (name, lon, lat, timestamp) VALUES ('".$nameIMO."','".$item['LON']."', '".$item['LAT']."', '".$item['TIMESTAMP']."')";
-          
           if (mysqli_query($conn, $sql)) 
           {
               echo "New record created successfully<br>";
@@ -90,7 +95,7 @@
           }
         }
       ?>
-      
+
        
     </pre>
   </body>
