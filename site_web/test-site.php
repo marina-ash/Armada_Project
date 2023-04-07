de440ef479353bafa242b1e04ae8df12bdd0f8c7
Marina ASHRAF MORIS <119459259+Marina-ASH@users.noreply.github.com>
2023-04-07 17:26:10 +0200

    test new desgin web

---
+++ site_web/test-site.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Armada</title>
+        <link rel="stylesheet" href="stile.css" type="text/css" media="screen">
+    </head>
+    <body >
+        <header>
+            <img class="logo" src="img/logo.png">   
+        </header> 
+
+        <div class="fond-1" id="accueil">
+            <nav id="bar">
+                <ul class="menu-bar">
+                    <li><a href="#accueil">Accueil</a></li>
+                    <li><a href="#carte">Carte Maps</a></li>
+                    <li><a href="#reservation">Réservations</a></li>
+                    <li><a href="#presentation">Qui sommes nous?</a></li>
+                </ul>
+            </nav>
+
+            <div id="colonne1">
+                <h1> 
+                    L’ARMADA, UN PEU D’HISTOIRE, LA NAISSANCE D’UN GRAND ÉVÉNEMENT 
+                </h1>
+                </div>
+                <div class="container">
+                    <div class="cercle"></div>
+                    <strong>Bienvenue sur notre site web dédié aux bateaux de l'armada ! Ici, vous pouvez découvrir la localisation en temps réel de tous les bateaux, et même réserver votre place à bord de l'un d'entre eux. Que vous soyez un passionné de navigation ou simplement à la recherche d'une expérience unique, nous sommes ravis de vous offrir un accès privilégié à cet événement exceptionnel. Notre équipe est à votre disposition pour vous guider et vous aider à profiter au maximum de votre visite. Nous vous souhaitons une excellente découverte de notre site et espérons vous voir bientôt à bord de l'un des</strong>
+                </div>
+
+                <div id="colonne3">
+                    <h1>
+                        L’ARMADA : UNE RÉUSSITE DEPUIS 30 ANS : DÉJÀ 6 ÉDITIONS !
+                    </h1>
+                </div>
+            </div>
+            
+        </div>
+
+
+    </body>
+</html>
